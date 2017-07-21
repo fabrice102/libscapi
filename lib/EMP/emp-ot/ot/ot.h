27da62e00e273092975e70edbab795d1d8fe913b
Fabrice Benhamouda <fabrice.benhamouda@normalesup.org>
2017-07-21 19:27:51 -0400

    Update to latest version of emp-ot

--- lib/EMP/emp-ot/ot/ot.h
+++ lib/EMP/emp-ot/ot/ot.h
@@ -1,12 +1,10 @@
 #ifndef OT_H__
 #define OT_H__
-#include </usr/local/include/emp-tool/emp-tool>
+#include <emp-tool>
 
 template<typename T>
 class OT {
 public:
-	OT(NetIO * nio): io(nio){};
-	NetIO* io = nullptr;
 	void send(const block* data0, const block* data1, int length) {
 		static_cast<T*>(this)->send_impl(data0, data1, length);
 	}
